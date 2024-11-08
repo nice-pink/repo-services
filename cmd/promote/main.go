99b28b0d2b8c45992a19c15210e5239945428ca4
nice-pink <r@nice.pink>
2024-11-08 14:47:31 +0100

    feat: Move promote to runner pkg.

--- cmd/promote/main.go
+++ cmd/promote/main.go
@@ -5,9 +5,7 @@ import (
 	"os"
 
 	"github.com/nice-pink/goutil/pkg/log"
-	"github.com/nice-pink/goutil/pkg/repo"
-	"github.com/nice-pink/repo-services/pkg/exceptional"
-	"github.com/nice-pink/repo-services/pkg/manifest"
+	"github.com/nice-pink/repo-services/pkg/runner"
 	"github.com/nice-pink/repo-services/pkg/util"
 )
 
@@ -27,36 +25,8 @@ func main() {
 	log.Info("*** Start")
 	log.Info(os.Args)
 
-	eh := exceptional.NewExceptionalHandler(*flags.ExceptionalAppsFile)
-	handler := manifest.NewManifestHandler(eh)
-
-	// build dest app
-	app := handler.BuildApp(flags, "")
-	currentTag := handler.GetCurrentTag(app)
-	if currentTag == "" {
-		log.Error("Can't get current tag.")
+	if err := runner.Promote(*srcEnv, *flags.ExceptionalAppsFile, flags, gitFlags); err != nil {
 		os.Exit(2)
-	}
-
-	// build src app - overwrite env
-	flags.Env = srcEnv
-	src := handler.BuildApp(flags, "")
-
-	// log apps and set tag with source
-	log.Info("Src app:", util.GetAppDescription(src))
-	log.Info("Dest app:", util.GetAppDescription(app))
-	if !handler.SetTagWithSource(src, app) {
-		os.Exit(2)
-	}
-
-	if *gitFlags.GitPush {
-		log.Info("Push to git.")
-		repoHandle := repo.NewRepoHandle(*gitFlags.SshKeyPath, *gitFlags.GitUser, *gitFlags.GitEmail)
-		msg := "Promote " + app.Name + "(" + app.Env + ") version: " + src.Tag
-		err := repoHandle.CommitPushLocalRepo(*flags.SrcPath, msg, true)
-		if err != nil {
-			os.Exit(2)
-		}
 	}
 }
 
--- pkg/runner/promote.go
+++ pkg/runner/promote.go
@@ -1,1 +1,45 @@
 package runner
+
+import (
+	"errors"
+
+	"github.com/nice-pink/goutil/pkg/log"
+	"github.com/nice-pink/goutil/pkg/repo"
+	"github.com/nice-pink/repo-services/pkg/exceptional"
+	"github.com/nice-pink/repo-services/pkg/manifest"
+	"github.com/nice-pink/repo-services/pkg/util"
+)
+
+func Promote(srcEnv, exceptionalAppsFile string, flags util.GeneralFlags, gitFlags util.GitFlags) error {
+	eh := exceptional.NewExceptionalHandler(exceptionalAppsFile)
+	handler := manifest.NewManifestHandler(eh)
+
+	// build dest app
+	app := handler.BuildApp(flags, "")
+	currentTag := handler.GetCurrentTag(app)
+	if currentTag == "" {
+		log.Error("Can't get current tag.")
+		return errors.New("no current tag")
+	}
+
+	// build src app - overwrite env
+	flags.Env = &srcEnv
+	src := handler.BuildApp(flags, "")
+
+	// log apps and set tag with source
+	log.Info("Src app:", util.GetAppDescription(src))
+	log.Info("Dest app:", util.GetAppDescription(app))
+	if !handler.SetTagWithSource(src, app) {
+		return errors.New("cannot set tag")
+	}
+
+	if *gitFlags.GitPush {
+		log.Info("Push to git.")
+		repoHandle := repo.NewRepoHandle(*gitFlags.SshKeyPath, *gitFlags.GitUser, *gitFlags.GitEmail)
+		msg := "Promote " + app.Name + "(" + app.Env + ") version: " + src.Tag
+		if err := repoHandle.CommitPushLocalRepo(*flags.SrcPath, msg, true); err != nil {
+			return err
+		}
+	}
+	return nil
+}
